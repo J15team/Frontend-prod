93c229a9d31ac0220302cd41dc8f9743b9de4db2
Admin SABAKAN <ka392561@gmail.com>
2026-01-09 21:33:28 +0900

    CSSファイルを分割して整理
    - variables.css: テーマカラー変数
    - global.css: グローバルスタイル
    - subjects.css: 題材一覧ページ
    - sections.css: セクションページ
    - profile.css: プロフィールページ
    - admin.css: 管理者ページ
    - components.css: 共通コンポーネント
    - animations.css: アニメーション効果

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/variables.css';
 import './styles/global.css';
-import './styles/index.css';
 import './styles/subjects.css';
+import './styles/sections.css';
+import './styles/profile.css';
+import './styles/admin.css';
+import './styles/components.css';
+import './styles/animations.css';
 import './styles/landing.css';
 import './styles/auth.css';
 
