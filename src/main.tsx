040d9b06eda92e73838566b0aff040bb84026662
Admin SABAKAN <ka392561@gmail.com>
2026-01-09 21:26:23 +0900

    リファクタ: CSSファイル分割（variables, global, subjects）

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles/variables.css';
+import './styles/global.css';
 import './styles/index.css';
+import './styles/subjects.css';
 import './styles/landing.css';
 import './styles/auth.css';
 
